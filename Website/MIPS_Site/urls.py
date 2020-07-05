037094727e0d96f256c9c69859b43c77189ff463
shag527 <shagunkatoch527@gmail.com>
2020-07-05 13:39:55 +0530

    Cache theory added

--- Website/MIPS_Site/urls.py
+++ Website/MIPS_Site/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('home',views.home,name="home"),
     path('doc',views.ViewDoc,name="doc"),
-    path('doc1',views.ViewDoc1,name="doc1"),
     path('doc2',views.ViewDoc2,name="doc2"),
     path('doc3',views.ViewDoc3,name="doc3"),
     path('projreport',views.ProjectReport,name="projreport"),
